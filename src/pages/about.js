fcad2c77a4168942143700fe801d552ca1f1d879
callihiggins <calli@nyu.edu>
2022-05-12 23:45:31 -0400

    woops more about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,1 +1,20 @@
 
+import * as React from 'react';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faSpinner, faPlus, faMinus, faEnvelope, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+import About from '../components/About';
+import "./base.css"
+
+library.add(fab, faSpinner, faPlus, faMinus, faEnvelope, faTimes);
+
+
+const AboutPage = () => {
+  return (
+    <About />
+  )
+}
+
+export default AboutPage;
