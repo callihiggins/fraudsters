5c62ffb61ca934cc8acb4fd777ec074e21722fc4
callihiggins <calli@nyu.edu>
2022-05-19 15:39:50 -0400

    try updating plugin

--- plugins/gatsby-source-simplecast/gatsby-node.js
+++ plugins/gatsby-source-simplecast/gatsby-node.js
@@ -1,44 +1,51 @@
 const createNodeHelpers = require('gatsby-node-helpers').default;
-const { createRemoteFileNode } = require('gatsby-source-filesystem')
+
+const {
+  createRemoteFileNode
+} = require('gatsby-source-filesystem');
+
 const Simplecast = require('./lib/Simplecast');
-const { createNodeFactory } = createNodeHelpers({ typePrefix: `Simplecast` });
+
+const {
+  createNodeFactory
+} = createNodeHelpers({
+  typePrefix: `Simplecast`
+});
 const PodcastEpisodeNode = createNodeFactory('PodcastEpisode', node => {
   return node;
 });
-
 const PLUGIN_NAME = 'gatsby-source-simplecast';
 const DEFAULTS = {
   fetchLimit: 99
 };
 
-
-exports.sourceNodes = async (
-  { actions: { createNode, setPluginStatus } },
-  { token, podcastId, fetchLimit = DEFAULTS.fetchLimit }
-) => {
-  const errorAboutGatsbyPlugins =
-    'To learn more about configuring Gatsby plugins, visit at https://www.gatsbyjs.org/docs/using-a-plugin-in-your-site/.';
-  const errorAboutSimplecastAuth =
-    'To learn more about Simplecast authentication, visit https://help.simplecast.com/en/articles/2724796-simplecast-2-0-api.';
+exports.sourceNodes = async ({
+  actions: {
+    createNode,
+    setPluginStatus
+  }
+}, {
+  token,
+  podcastId,
+  fetchLimit = DEFAULTS.fetchLimit
+}) => {
+  const errorAboutGatsbyPlugins = 'To learn more about configuring Gatsby plugins, visit at https://www.gatsbyjs.org/docs/using-a-plugin-in-your-site/.';
+  const errorAboutSimplecastAuth = 'To learn more about Simplecast authentication, visit https://help.simplecast.com/en/articles/2724796-simplecast-2-0-api.';
   const errorAboutPodcastId = `To get your podcast ID, login to Simplecast, click 'Show Settings' from your account dashboard.`;
-
-  if (!token)
-    throw new Error(
-      `It looks like you forgot your Simplecast Auth token! Make sure to pass your token into '${PLUGIN_NAME}' options in 'gatsby-config.js'. \n${errorAboutSimplecastAuth} \n${errorAboutGatsbyPlugins}`
-    );
-  if (!podcastId)
-    throw new Error(
-      `It looks like you forgot your Simplecast Podcast ID! Make sure to pass the ID into '${PLUGIN_NAME}' options in 'gatsby-config.js'. \n${errorAboutPodcastId} \n${errorAboutGatsbyPlugins}`
-    );
+  if (!token) throw new Error(`It looks like you forgot your Simplecast Auth token! Make sure to pass your token into '${PLUGIN_NAME}' options in 'gatsby-config.js'. \n${errorAboutSimplecastAuth} \n${errorAboutGatsbyPlugins}`);
+  if (!podcastId) throw new Error(`It looks like you forgot your Simplecast Podcast ID! Make sure to pass the ID into '${PLUGIN_NAME}' options in 'gatsby-config.js'. \n${errorAboutPodcastId} \n${errorAboutGatsbyPlugins}`);
 
   try {
-    const sc = new Simplecast({ token, podcastId });
+    const sc = new Simplecast({
+      token,
+      podcastId
+    });
     const episodes = await sc.getEpisodes(fetchLimit);
     const episodesWithKeywords = await Promise.all(episodes.map(async episode => {
-        const keywordNodes = await sc.getEpisodeKeywords(episode.id);
-        const keyWordNodeValues = keywordNodes.collection.map(keyword => keyword.value);
-        episode.keywords = keyWordNodeValues;
-        return episode;
+      const keywordNodes = await sc.getEpisodeKeywords(episode.id);
+      const keyWordNodeValues = keywordNodes.collection.map(keyword => keyword.value);
+      episode.keywords = keyWordNodeValues;
+      return episode;
     }));
     const episodesWithSearchData = await Promise.all(episodesWithKeywords.map(async episode => {
       const episodeNode = await sc.getEpisodeSearchData(episode.slug);
@@ -46,26 +53,25 @@ exports.sourceNodes = async (
       episode.authors = episodeNode.authors?.collection?.map(author => author.name);
       return episode;
     }));
-    episodesWithSearchData
-      .map(episode => {
-        return PodcastEpisodeNode(episode)
-      })
-     .forEach(node => createNode(node));
-
-    setPluginStatus({ lastFetched: Date.now() });
+    episodesWithSearchData.map(episode => {
+      return PodcastEpisodeNode(episode);
+    }).forEach(node => createNode(node));
+    setPluginStatus({
+      lastFetched: Date.now()
+    });
   } catch (err) {
     console.error('FAIL:', err);
   }
 };
 
-const nodeWithImage = ['SimplecastPodcastEpisode']
+const nodeWithImage = ['SimplecastPodcastEpisode'];
 
 exports.onCreateNode = async ({
   node,
   actions,
   store,
   cache,
-  createNodeId,
+  createNodeId
 }) => {
   if (nodeWithImage.includes(node.internal.type) && node.imageUrl) {
     const fileNode = await createRemoteFileNode({
@@ -74,18 +80,20 @@ exports.onCreateNode = async ({
       createNode: actions.createNode,
       createNodeId,
       cache,
-      store,
-    })
+      store
+    });
+
     if (fileNode) {
-      node.image___NODE = fileNode.id
+      node.image___NODE = fileNode.id;
     }
   }
-}
+};
 
-exports.createSchemaCustomization = ({ actions, schema }) => {
-  actions.createTypes(
-    `type SimplecastPodcastEpisode implements Node {
+exports.createSchemaCustomization = ({
+  actions,
+  schema
+}) => {
+  actions.createTypes(`type SimplecastPodcastEpisode implements Node {
       image: File @link(from: "image___NODE")
-    }`
-  );
+    }`);
 };
--- plugins/gatsby-source-simplecast/lib/mapObj.js
+++ plugins/gatsby-source-simplecast/lib/mapObj.js
@@ -1,69 +1,66 @@
-const isObject = value => typeof value === 'object' && value !== null;
+const isObject = value => typeof value === 'object' && value !== null; // Customized for this use-case
 
-// Customized for this use-case
-const isObjectCustom = value =>
-	isObject(value)
-	&& !(value instanceof RegExp)
-	&& !(value instanceof Error)
-	&& !(value instanceof Date);
+
+const isObjectCustom = value => isObject(value) && !(value instanceof RegExp) && !(value instanceof Error) && !(value instanceof Date);
 
 const mapObjectSkip = Symbol('mapObjectSkip');
 
 const _mapObject = (object, mapper, options, isSeen = new WeakMap()) => {
-	options = {
-		deep: false,
-		target: {},
-		...options,
-	};
+  options = {
+    deep: false,
+    target: {},
+    ...options
+  };
 
-	if (isSeen.has(object)) {
-		return isSeen.get(object);
-	}
+  if (isSeen.has(object)) {
+    return isSeen.get(object);
+  }
 
-	isSeen.set(object, options.target);
+  isSeen.set(object, options.target);
+  const {
+    target
+  } = options;
+  delete options.target;
 
-	const {target} = options;
-	delete options.target;
+  const mapArray = array => array.map(element => isObjectCustom(element) ? _mapObject(element, mapper, options, isSeen) : element);
 
-	const mapArray = array => array.map(element => isObjectCustom(element) ? _mapObject(element, mapper, options, isSeen) : element);
-	if (Array.isArray(object)) {
-		return mapArray(object);
-	}
+  if (Array.isArray(object)) {
+    return mapArray(object);
+  }
 
-	for (const [key, value] of Object.entries(object)) {
-		const mapResult = mapper(key, value, object);
+  for (const [key, value] of Object.entries(object)) {
+    const mapResult = mapper(key, value, object);
 
-		if (mapResult === mapObjectSkip) {
-			continue;
-		}
+    if (mapResult === mapObjectSkip) {
+      continue;
+    }
 
-		let [newKey, newValue, {shouldRecurse = true} = {}] = mapResult;
+    let [newKey, newValue, {
+      shouldRecurse = true
+    } = {}] = mapResult; // Drop `__proto__` keys.
 
-		// Drop `__proto__` keys.
-		if (newKey === '__proto__') {
-			continue;
-		}
+    if (newKey === '__proto__') {
+      continue;
+    }
 
-		if (options.deep && shouldRecurse && isObjectCustom(newValue)) {
-			newValue = Array.isArray(newValue)
-				? mapArray(newValue)
-				: _mapObject(newValue, mapper, options, isSeen);
-		}
+    if (options.deep && shouldRecurse && isObjectCustom(newValue)) {
+      newValue = Array.isArray(newValue) ? mapArray(newValue) : _mapObject(newValue, mapper, options, isSeen);
+    }
 
-		target[newKey] = newValue;
-	}
+    target[newKey] = newValue;
+  }
 
-	return target;
+  return target;
 };
 
 module.exports = function mapObject(object, mapper, options) {
-	if (!isObject(object)) {
-		throw new TypeError(`Expected an object, got \`${object}\` (${typeof object})`);
-	}
+  if (!isObject(object)) {
+    throw new TypeError(`Expected an object, got \`${object}\` (${typeof object})`);
+  }
 
-	if (Array.isArray(object)) {
-		throw new TypeError('Expected an object, got an array');
-	}
+  if (Array.isArray(object)) {
+    throw new TypeError('Expected an object, got an array');
+  }
 
-	return _mapObject(object, mapper, options);
-}
+  return _mapObject(object, mapper, options);
+};
