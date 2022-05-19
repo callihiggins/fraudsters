890f46d4433c720b12f8c18c6e2041e621545c05
callihiggins <calli@nyu.edu>
2022-05-19 18:48:36 -0400

    wow this was not updating

--- plugins/gatsby-source-simplecast/lib/Simplecast.js
+++ plugins/gatsby-source-simplecast/lib/Simplecast.js
@@ -48,18 +48,20 @@ class Simplecast {
       throw Error('No episode ID provided.');
     }
 
-    return this.request(`episodes/${this.podcastId}/episodes`).then(res => res.json()).then(data => camelCaseKeys(data, {
-      deep: true
-    })).catch(console.error);
+    return this.request(`episodes/${this.podcastId}/episodes`).then(res => res.json()).then(data => {
+      throw Error(data);
+    }) //  camelCaseKeys(data, { deep: true })})
+    .catch(console.error);
   };
   getEpisodeKeywords = episodeId => {
     if (!episodeId) {
       throw Error('No episode ID provided.');
     }
 
-    return this.request(`episodes/${episodeId}/keywords`).then(res => res.json()).then(data => camelCaseKeys(data, {
-      deep: true
-    })).catch(console.error);
+    return this.request(`episodes/${episodeId}/keywords`).then(res => res.json()).then(data => {
+      throw Error(data);
+    }) //  camelCaseKeys(data, { deep: true })})
+    .catch(console.error);
   };
   getEpisodeSearchData = slug => {
     if (!slug) {
@@ -74,20 +76,31 @@ class Simplecast {
       headers: this.headers,
       cache: 'default',
       body: JSON.stringify(payload)
-    }).then(res => res.json()).then(data => camelCaseKeys(data, {
-      deep: true
-    })).catch(console.error);
+    }).then(res => res.json()).then(data => {
+      throw Error(data);
+    }) //  camelCaseKeys(data, { deep: true })})
+    .catch(() => {
+      throw Error(error);
+    });
     ;
   };
   getShowInfo = () => {
-    return this.request(`podcasts/${this.podcastId}`).then(res => res.json()).then(data => camelCaseKeys(data, {
-      deep: true
-    })).catch(console.error);
+    return this.request(`podcasts/${this.podcastId}`).then(res => res.json()).then(data => {
+      throw Error(data);
+    }) //  camelCaseKeys(data, { deep: true })})
+    .catch(() => {
+      throw Error(error);
+    });
+    ;
   };
   getEpisodes = (limit = 10) => {
-    return this.request(`podcasts/${this.podcastId}/episodes?limit=${typeof limit === 'number' ? limit : 10}`).then(res => res.json()).then(info => info.collection).then(data => camelCaseKeys(data, {
-      deep: true
-    })).catch(console.error);
+    return this.request(`podcasts/${this.podcastId}/episodes?limit=${typeof limit === 'number' ? limit : 10}`).then(res => res.json()).then(info => info.collection).then(data => {
+      throw Error(data);
+    }) //  camelCaseKeys(data, { deep: true })})
+    .catch(() => {
+      throw Error(error);
+    });
+    ;
   };
 }
 
