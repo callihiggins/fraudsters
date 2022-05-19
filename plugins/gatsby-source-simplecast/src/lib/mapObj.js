a70b6965b83e3f3f2439c3038014f45a84cd2e81
callihiggins <calli@nyu.edu>
2022-05-19 18:39:15 -0400

    more commenting out

--- plugins/gatsby-source-simplecast/src/lib/mapObj.js
+++ plugins/gatsby-source-simplecast/src/lib/mapObj.js
@@ -56,14 +56,14 @@ const _mapObject = (object, mapper, options, isSeen = new WeakMap()) => {
 	return target;
 };
 
-module.exports = function mapObject(object, mapper, options) {
-	if (!isObject(object)) {
-		throw new TypeError(`Expected an object, got \`${object}\` (${typeof object})`);
-	}
+// module.exports = function mapObject(object, mapper, options) {
+// 	if (!isObject(object)) {
+// 		throw new TypeError(`Expected an object, got \`${object}\` (${typeof object})`);
+// 	}
 
-	if (Array.isArray(object)) {
-		throw new TypeError('Expected an object, got an array');
-	}
+// 	if (Array.isArray(object)) {
+// 		throw new TypeError('Expected an object, got an array');
+// 	}
 
-	return _mapObject(object, mapper, options);
-}
+// 	return _mapObject(object, mapper, options);
+// }
